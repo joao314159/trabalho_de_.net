de7761c95b8a60f086350cde6d68c2b35c97d185
Warley Ramires <warleyramiresgoncalves@gmail.com>
2023-11-17 16:14:28 -0300

    Tentativa de adicionar rota para pagina de login

--- TrabConteudoAcademico/Program.cs
+++ TrabConteudoAcademico/Program.cs
@@ -25,7 +25,13 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+//app.MapBlazorHub();
+//app.MapFallbackToPage("/_Host");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapBlazorHub();
+    endpoints.MapFallbackToPage("/_Host");
+});
 
 app.Run();
